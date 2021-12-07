69106144ce716c8ebd51e2e9939759a95f167e8f
David Melsheimer <dave.melshman@gmail.com>
2021-12-07 10:04:44 -0500

    Self Executing Anonymous Functions, SEAF!

---
+++ immediately_invoked_function_expression.js
@@ -0,0 +1,31 @@
+//both of these (#1 and #2) work in the browser, despite the difference
+//in nesting of the parenthesis
+
+//#1
+(function printMeTenTimes(){
+    let i = 0;
+    while(i<10){
+        console.log(`printing ${i}`);
+        i++;
+    }
+})()
+//#2
+(function printMeTenTimes(){
+    let i = 0;
+    while(i<10){
+        console.log(`printing ${i}`);
+        i++;
+    }
+})()
+
+
+
+
+//oh!  It also works as anonymous (SEAF_ Self Executing Anonymous Function)
+(function(){
+    let i = 0;
+    while(i<10){
+        console.log(`printing ${i}`);
+        i++;
+    }
+})()

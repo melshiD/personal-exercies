15a027bb3bce8b2cce8905db387e1637f9f97a0b
David Melsheimer <dave.melshman@gmail.com>
2022-01-13 15:21:10 -0500

    working with closure.  Want to write a function to store anime.timeline in protected state

---
+++ closure_counter/closure_counter.js
@@ -0,0 +1,50 @@
+// Global function which would form
+// closure with modify function
+function counter() {
+  
+    // Private counter variable
+    let count = 0;
+    
+    // To increment the value of counter
+    function increment() {
+      count++;
+    }
+    
+    // To decrement the value of counter
+    function decrement() {
+      count--;
+    }
+    
+    // Modify function forms closure
+    // here which is used outside
+    function modify(val) {
+    
+      // To check increment or decrement
+      // button has been clicked
+      if (val === "1") increment();
+      else if (val === "0") decrement();
+    
+      // Return the counter
+      return count;
+    }
+    
+    // Returning to make it available
+    // outside counter function
+    return modify;
+  }
+    
+  // Storing the closure modify
+  const closure = counter();
+    
+  // This function handles the button
+  // click, objButton to get value
+  function counterHandler(objButton) {
+    
+    // Storing the value returned by modify
+    let count = closure(objButton.value);
+    
+    // Getting div by it's id
+    // and modifying its inner html
+    document.getElementById("counter_div")
+      .innerHTML = "<h2>" + count + "</h2>";
+  }

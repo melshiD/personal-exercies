bc022b8d6ab0d865494a6a87038d317d502c27f1
David Melsheimer <dave.melshman@gmail.com>
2022-01-15 17:22:02 -0500

    working with codepen of gsap timeline

--- SVG_followalong/svg_followalong.js
+++ SVG_followalong/svg_followalong.js
@@ -1,25 +1,48 @@
 var tl = new TimelineMax(),
          box = document.querySelectorAll('.box');
 
+tl.add('beginning');
+
 tl.staggerFrom(box, 1, {
     x: -200, ease: Bounce.easeOut
-}, 0.1)
+}, 0.1);
+
 tl.staggerFrom(box, 1, {
     rotation: 90, ease: Bounce.easeOut, delay: 1
-}, 0.1)
+}, 0.1);
+
 tl.add('test_tag');
 
 tl.staggerTo(box, 1, {
     x: '300%', ease: Bounce.easeOut
 }, 0.1, '-=0.4')
 
-tl.staggerTo(box, 3, { rotation: 1080}, 0.3);
+tl.staggerTo(box, 3, {
+    rotation: 1080,
+    borderColor: 'black',
+}, 0.3);
+
+tl.add('move_svgs');
+
 
 tl.staggerTo(box, 2, {
     scale: 2,
     ease: 'Bounce.inOut'
 }, 1, "test_tag");
+
 tl.to('.new-box', 1, {
-    skew: 35,
     scaleX: 2
 }, 'test_tag-=0.5');
+
+tl.to('#main_svg', 3, {
+    y: 500
+}, 'test_tag-=0.2');
+
+tl.to('.box', 3, {
+    scale: 0.7
+}, '<2');
+
+tl.to('#main_svg_2', 2, {
+    y: -500
+}, 'beginning-=.5');
+

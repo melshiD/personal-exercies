8a378b07063e682ba906709892046c6dc779bf16
David Melsheimer <dave.melshman@gmail.com>
2022-01-16 13:23:45 -0500

    adding click-to-move-playhead functionality best I can

--- timeline_visualizer_gsap/timeline_gsap.js
+++ timeline_visualizer_gsap/timeline_gsap.js
@@ -1,14 +1,14 @@
 var pixelsPerSecond = 200;
 var animation = gsap.timeline();
 animation
-    .to("#star", {duration:20, x:1150})
+    .to("#star", {duration:2, x:1150})
     .to("#circle", {duration:1, x:1150})
-    .to("#square", {duration:3, x:1150}, "<-2")
+    .to("#square", {duration:3, x:1150});
 
 
 
 animation.eventCallback("onUpdate", movePlayhead).paused(true);
-gsap.to("svg", {autoAlpha:1})
+gsap.to("svg", {autoAlpha:1});
 var time = document.getElementById("time")
 var maxX = animation.duration() * pixelsPerSecond;
 
@@ -19,14 +19,19 @@ for(var i = 0; i < numChildren; i++){
   gsap.set("#tween" + i, {x:children[i].startTime() * pixelsPerSecond})
   gsap.set("#rect" + i, {width:children[i].duration() * pixelsPerSecond})
 }
+
+var playHead = document.getElementById('timeline');
+playHead.addEventListener('mousedown', handleClick);
+
 var dragger = Draggable.create("#playhead", {
   type:"x", 
-  
   cursor:"pointer",
   trigger:"#timeline",
   bounds: {minX:0, maxX:maxX},
   onDrag:function(){
     animation.pause();
+    // gsap.set("#playhead", {this * maxX});
+
     time.textContent=animation.time().toFixed(1);
     animation.progress(this.x/maxX);
   }
@@ -50,3 +55,14 @@ document.getElementById("reverse").onclick = function() {
   animation.reverse();
 }
 
+function handleClick(e){
+    let screenWidth = window.innerWidth,
+        timelineWidth = document.getElementById('tween_x5F_bg').getBoundingClientRect().width,
+        timelineOffset = document.getElementById('tween_x5F_bg').getBoundingClientRect().x;
+    
+    console.log(timelineWidth, timelineOffset, e.offsetX);
+    animation.pause();
+    gsap.set("#playhead", {x:`${(e.offsetX)}`});
+    // time.textContent=animation.time().toFixed(1); 
+    // movePlayhead();
+}

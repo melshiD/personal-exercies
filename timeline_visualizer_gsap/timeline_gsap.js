426dc9e88ba66b7fac9d3e99db8f893250810505
David Melsheimer <dave.melshman@gmail.com>
2022-01-17 18:46:23 -0500

    thank goodness.  The click-move-shuttle feature works.  Could be better by not needing an extra click to start dragging

--- timeline_visualizer_gsap/timeline_gsap.js
+++ timeline_visualizer_gsap/timeline_gsap.js
@@ -1,16 +1,17 @@
+
+
 var pixelsPerSecond = 200;
 var animation = gsap.timeline();
+gsap.to("svg", {autoAlpha:1});
 animation
     .to("#star", {duration:4, x:1150}, 1)
     .to("#circle", {duration:1, x:1150}, 1)
     .to("#square", {duration:3, x:1150}, 1);
 
-
-
 animation.eventCallback("onUpdate", movePlayhead).paused(true);
-gsap.to("svg", {autoAlpha:1});
 var time = document.getElementById("time")
-var maxX = animation.duration() * pixelsPerSecond;
+// var maxX = animation.duration() * pixelsPerSecond;
+let maxX = 1200;
 
 var children = animation.getChildren()
 var numChildren = children.length;
@@ -27,6 +28,7 @@ var dragger = Draggable.create("#playhead", {
   cursor:"pointer",
   trigger:"#playhead",
   bounds: {minX:0, maxX:maxX},
+  // onPress: handleClick(),
   onDrag:function(){
     animation.pause();
     time.textContent=animation.time().toFixed(1);
@@ -38,6 +40,8 @@ svgTimeline.addEventListener('mousedown', handleClick);
 
 
 function movePlayhead() {
+  //multiplication factor needs to match the svg length of the playhead
+  console.log('registered update: x='+ animation.progress() * maxX);
   gsap.set("#playhead", {x:animation.progress() * maxX});
   time.textContent=animation.time().toFixed(1); 
 }
@@ -56,9 +60,17 @@ document.getElementById("reverse").onclick = function() {
 
 function handleClick(e) {
   let newX = findPercentIntoTimeline(e.offsetX);
-  // animation.pause();
-  // gsap.set("#playhead", { x: newX*1200 });
+
+  console.log('percent into timeline: ' + newX, 
+              'times 1200: ' + newX*1200, 
+              'timeline.boundingRect.x: ' + svgTimeline.getBoundingClientRect().x, 
+              'event.offsetX: ' + e.offsetX);
+
+
+  animation.pause();
+  gsap.set("#playhead", { x: newX*1200 });
   animation.progress(newX);
+  // movePlayhead();
 }
 
 function findPercentIntoTimeline(offsetX) {

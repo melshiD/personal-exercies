8bda4d1cd5432f9f8dfea1fc726a82f79efc29d7
David Melsheimer <dave.melshman@gmail.com>
2022-01-17 22:24:47 -0500

    sloved the duration vs timeline length

--- timeline_visualizer_gsap/timeline_gsap.js
+++ timeline_visualizer_gsap/timeline_gsap.js
@@ -4,14 +4,16 @@ var pixelsPerSecond = 200;
 var animation = gsap.timeline();
 gsap.to("svg", {autoAlpha:1});
 animation
-    .to("#star", {duration:4, x:1150}, 1)
+    .to("#star", {duration:5, x:1150}, 0)
     .to("#circle", {duration:1, x:1150}, 1)
-    .to("#square", {duration:3, x:1150}, 1);
+    .to("#square", {duration:1, x:1150}, 1);
 
 animation.eventCallback("onUpdate", movePlayhead).paused(true);
+
 var time = document.getElementById("time")
-// var maxX = animation.duration() * pixelsPerSecond;
-let maxX = 1200;
+var duration = animation.duration() * pixelsPerSecond;
+let maxX = document.getElementById('tween_x5F_bg').getBoundingClientRect().width;
+console.log(maxX);
 
 var children = animation.getChildren()
 var numChildren = children.length;
@@ -42,7 +44,8 @@ svgTimeline.addEventListener('mousedown', handleClick);
 function movePlayhead() {
   //multiplication factor needs to match the svg length of the playhead
   console.log('registered update: x='+ animation.progress() * maxX);
-  gsap.set("#playhead", {x:animation.progress() * maxX});
+  gsap.set("#playhead", {x:animation.progress() * duration});
+  console.log(animation.progress());
   time.textContent=animation.time().toFixed(1); 
 }
 
@@ -60,23 +63,30 @@ document.getElementById("reverse").onclick = function() {
 
 function handleClick(e) {
   let newX = findPercentIntoTimeline(e.offsetX);
-
+  let xOffset = document.getElementById('tween_x5F_bg').getBoundingClientRect().x
+  let percentIntoDuration =  (duration - xOffset) / e.offsetX;
   console.log('percent into timeline: ' + newX, 
               'times 1200: ' + newX*1200, 
               'timeline.boundingRect.x: ' + svgTimeline.getBoundingClientRect().x, 
               'event.offsetX: ' + e.offsetX);
-
-
   animation.pause();
-  gsap.set("#playhead", { x: newX*1200 });
+
+  //here is where the persumed length of the playhead and duration of the 
+  //animation can confict to cause the time to not track with the palyhead past the 
+  //set animations
+  gsap.set("#playhead", { x: newX });
   animation.progress(newX);
   // movePlayhead();
 }
 
 function findPercentIntoTimeline(offsetX) {
+  // let timelineWidth = document.getElementById('timeline').getBoundingClientRect().width,
+  //   timelineOffset = document.getElementById('timeline').getBoundingClientRect().x;
   let timelineWidth = document.getElementById('tween_x5F_bg').getBoundingClientRect().width,
     timelineOffset = document.getElementById('tween_x5F_bg').getBoundingClientRect().x;
   let xIntoRect = offsetX - timelineOffset;
   let percentIntoTimeline = xIntoRect / timelineWidth;
   return percentIntoTimeline;
 }
+
+//percent into playhead assumes annimation goes all the way to end of playhead

cd79a5315a1c5455dba18577a213a5d1aff78832
David Melsheimer <dave.melshman@gmail.com>
2022-01-18 16:05:06 -0500

    i broke it again.  I need to put this down for a bit

--- timeline_visualizer_gsap/timeline_gsap.js
+++ timeline_visualizer_gsap/timeline_gsap.js
@@ -1,31 +1,33 @@
-
-
 var pixelsPerSecond = 200;
 var animation = gsap.timeline();
 let bodyDeltaBackground = currentMargin();
-var duration = animation.duration() * pixelsPerSecond;
 var svgTimeline = document.getElementById('timeline');
 svgTimeline.addEventListener('mousedown', handleClick);
-svgTimeline.addEventListener('wheel', handleWheel, {passive: true});
+svgTimeline.addEventListener('wheel', handleWheel, { passive: true });
 
-function currentMargin(){
+function currentMargin() {
   //manages the margins that may be between the SVG background and the body as a whole
-  let currentMargin = Math.floor(Math.abs((document.body.offsetWidth - 
-                                     document.getElementById('Layer_1')
-                                     .getBoundingClientRect().width)/2));
+  let currentMargin = Math.floor(Math.abs((document.body.offsetWidth -
+    document.getElementById('Layer_1')
+      .getBoundingClientRect().width) / 2));
   return currentMargin;
 }
 
-window.addEventListener('resize', () =>{
+window.addEventListener('resize', () => {
   bodyDeltaBackground = currentMargin();
 });
 
 console.log("HERE: " + bodyDeltaBackground);
-gsap.to("svg", {autoAlpha:1});
+gsap.to("svg", { autoAlpha: 1 });
 animation
-    .to("#star", {duration:5, x:1150}, 0)
-    .to("#circle", {duration:1, x:1150}, 1)
-    .to("#square", {duration:1, x:1150}, 1);
+  .to("#star", { duration: 6, x: 1150 }, 0)
+  .to("#circle", { duration: 1, x: 1150 }, 1)
+  .to("#square", { duration: 1, x: 1150 }, 1)
+  // .to("#cheap_tricks", {duration: 6, x: 0}, 0);
+
+var duration = animation.duration() * pixelsPerSecond;
+let portionOfPlaybackThatsAnimation = duration/document.getElementById('tween_x5F_bg').getAttribute('width');
+console.log("portionOfPlaybackThatsAnimation " + portionOfPlaybackThatsAnimation);
 
 animation.eventCallback("onUpdate", movePlayhead).paused(true);
 
@@ -33,76 +35,74 @@ var time = document.getElementById("time")
 
 // let maxX = 1200; looked to work because 6 sec x 200 is 1200
 let maxX = document.getElementById('tween_x5F_bg').getBoundingClientRect().width;
+let timelineScalingFactor = maxX/document.getElementById('tween_x5F_bg').getAttribute('width');
+let pixelsOfDuration = Math.floor(duration * timelineScalingFactor);
+console.log("pixels of duration :" + pixelsOfDuration);
+
 console.log(maxX);
 
 var children = animation.getChildren()
 var numChildren = children.length;
 
-for(var i = 0; i < numChildren; i++){
-  gsap.set("#tween" + i, {x:children[i].startTime() * pixelsPerSecond})
-  gsap.set("#rect" + i, {width:children[i].duration() * pixelsPerSecond})
+for (var i = 0; i < numChildren; i++) {
+  gsap.set("#tween" + i, { x: children[i].startTime() * pixelsPerSecond })
+  gsap.set("#rect" + i, { width: children[i].duration() * pixelsPerSecond })
 }
 
 
 
 var dragger = Draggable.create("#playhead", {
-  type:"x", 
-  cursor:"pointer",
-  trigger:"#playhead",
-  bounds: {minX:0, maxX:maxX},
+  type: "x",
+  cursor: "pointer",
+  trigger: "#playhead",
+  bounds: { minX: 0, maxX: maxX },
   // onPress: handleClick(),
-  onDrag:function(){
+  onDrag: function () {
     animation.pause();
-    time.textContent=animation.time().toFixed(1);
-    animation.progress(this.x/1000);
+    time.textContent = animation.time().toFixed(1);
+    animation.progress(this.x / 1000);
     //use a 'playableWidth' variable?
   }
 });
 
 function movePlayhead() {
   //multiplication factor needs to match the svg length of the playhead
-  console.log('registered update: x='+ animation.progress() * maxX);
-  gsap.set("#playhead", {x:animation.progress() * 1200});
-  console.log(animation.progress());
-  time.textContent=animation.time().toFixed(1); 
+  gsap.set("#playhead", { x: animation.progress() * duration });
+  time.textContent = animation.time().toFixed(2);
 }
 
-document.getElementById("play").onclick = function() {
+document.getElementById("play").onclick = function () {
   animation.play();
 }
 
-document.getElementById("pause").onclick = function() {
+document.getElementById("pause").onclick = function () {
   animation.pause();
 }
 
-document.getElementById("reverse").onclick = function() {
+document.getElementById("reverse").onclick = function () {
   animation.reverse();
 }
 
 function handleClick(e) {
-  let newX = findPercentIntoTimeline(e.offsetX);
+console.log(e.offsetX);
   let xOffset = document.getElementById('tween_x5F_bg').getBoundingClientRect().x
-  let percentIntoDuration =  (duration - xOffset) - e.offsetX;
-  console.log()
-  console.log('percent into timeline: ' + newX, 
-              'times 1200: ' + newX*1200, 
-              'timeline.boundingRect.x: ' + svgTimeline.getBoundingClientRect().x, 
-              'event.offsetX: ' + e.offsetX);
+  console.log("offset of tween_rect :" + xOffset);
+  let percentIntoDuration = clickInAnimation(e, xOffset);
+  
   animation.pause();
 
   //here is where the persumed length of the playhead and duration of the 
   //animation can confict to cause the time to not track with the palyhead past the 
   //set animations
-  gsap.set("#playhead", { x: newX});
-  animation.progress(newX);
+  gsap.set("#playhead", { x: percentIntoDuration*duration });//going a percent of duration?
+  animation.progress(percentIntoDuration);
   // movePlayhead();
 }
 
-function handleWheel(e){
+function handleWheel(e) {
   let degree = - (e.deltaY * 0.0001);
-  console.log(degree);
-  animation.progress(animation.progress() + degree);
-  console.log(animation.progress());
+  animation.progress(animation.progress() + degree).pause();
+
 }
 
 function findPercentIntoTimeline(offsetX) {
@@ -115,4 +115,11 @@ function findPercentIntoTimeline(offsetX) {
   return percentIntoTimeline;
 }
 
+function clickInAnimation(e, xOffset){
+  let xPositionClicked = e.offsetX - xOffset;
+  if(xPositionClicked >= pixelsOfDuration){
+    return 1;
+  }
+  return xPositionClicked/pixelsOfDuration;
+}
 //percent into playhead assumes annimation goes all the way to end of playhead

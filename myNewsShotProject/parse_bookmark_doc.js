ca878811b18b25203c1d18743ad952d031463fc7
David Melsheimer <dave.melshman@gmail.com>
2022-02-11 00:18:13 -0500

    just need some error handling to retrieve most screenshots

---
+++ myNewsShotProject/parse_bookmark_doc.js
@@ -0,0 +1,49 @@
+const { Console } = require('console');
+const fs = require('fs');
+const { parse } = require('path');
+// const writeStream = fs.createWriteStream('url_array.txt');
+// const pathName = writeStream.path
+async function parseBookmarkPage() {
+
+    const puppeteer = require('puppeteer');
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto('C:/Users/davem/Coding/personal_exercises/myNewsShotProject/bookmarks/bookmarks_2_10_22.html', { waitUntil: 'networkidle2' });
+
+    const linkList = await page.evaluate(() => {
+        let linkTags = document.querySelectorAll('a');
+        let links = [];
+        linkTags.forEach((link) => links.push(link.href));
+        return links;
+    });
+
+
+    let path = `C:/Users/davem/Coding/personal_exercises/myNewsShotProject/bookmarks/bookmark_url_arrays/url_array.txt`;
+
+    const writeToFile = fs.createWriteStream(path, {
+        flags: 'a'
+    });
+    for(let link of linkList){
+        writeToFile.write(`${link}\n`);
+    }
+
+
+    // fs.open(path, 'w', function(err, fd){
+    //     if(err){
+    //         throw 'could not open file: ' + err;
+    //     }
+    //     fs.write(fd, linkList[19], 0, 'utf8', function(err){
+    //         if(err) throw 'error writing file: ' +err;
+    //         fs.close(fd, function(){
+    //             console.log('wrote file successfully');
+    //         });
+    //     });
+    // });
+    browser.close();
+}
+
+parseBookmarkPage();
+
+//WHEN YOU SIT BACK DOWN, FIGURE OUT HOW TO OPEN AND CLOSE AND OPEN
+//THE PORT OR WHATEVER IT'S CALLED SO i CAN SAVE THE ARRAY LINE BY LINE, 
+//SO i CAN GIVE A TYPE string TO THE DATA ARGUMENT
--- myNewsShotProject/snapshot.js
+++ myNewsShotProject/snapshot.js
@@ -1,17 +1,11 @@
 //https://medium.com/@viviancpy/save-screenshot-of-websites-with-puppeteer-cloudinary-and-heroku-1-3-bba6082d21d0
 const puppeteer = require('puppeteer');
-
-
+const fs = require('fs');
 
 async function doScreenCapture(url, site_name) {
 
-  const d = new Date();
-  const current_time = `${d.getFullYear()}_${d.getMonth() + 1}_${d.getDate()}_${d.getHours()}_${d.getMinutes()}`
-
-  const cloudinary_options = {
-    public_id: `newsshot/${current_time}_${site_name}`,
-  };
-  console.log(cloudinary_options.public_id);
+  // const d = new Date();
+  // const current_time = `${d.getFullYear()}_${d.getMonth() + 1}_${d.getDate()}_${d.getHours()}_${d.getMinutes()}`
 
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
@@ -27,40 +21,15 @@ async function doScreenCapture(url, site_name) {
 }
 
 
-const newSites = [
-  {
-    name: 'nasa',
-    url: 'https://www.nasa.gov/'
-  }, {
-    name: 'coindesk',
-    url: 'https://www.coindesk.com/'
-  }, {
-    name: 'cointelegraph',
-    url: 'https://cointelegraph.com/'
-  }, {
-    name: 'reuters',
-    url: 'https://reuters.com/'
-  }
-];
+const newSites = fs.readFileSync('C:/Users/davem/Coding/personal_exercises/myNewsShotProject/bookmarks/bookmark_url_arrays/url_array.txt').toString('utf-8');
+let sitesByLine = newSites.split('\n');
+const loopAndGetSnapshot = async (siteList) => {
+  console.log('start');
 
-async function doSnapshots(new_site) {
-  let cloudinary_promises = [];
-  for (let site of new_sites) {
-    try {
-      let cloudinary_snapshot = await doScreenCapture(site['url'], site['name']);
-      if (cloudinary_snapshot) {
-        cloudinary_promises.push(cloudinary_snapshot);
-      }
-    } catch (e) {
-      console.error(`[${site['name'] || 'Unknown site'}] Error insnapshotting`, e);
-    }
+  for(let site of siteList){
+    await doScreenCapture(site, siteList.indexOf(site));
+    // console.log(site);
   }
-  Promise.all(cloudinary_promises).then(function (val) {
-    process.exit();
-  });
 }
 
-// doSnapshots(new_sites);
-for (let site of newSites) {
-  doScreenCapture(site['url'], site['name']);
-}
+loopAndGetSnapshot(sitesByLine);

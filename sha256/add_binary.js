e32688843df07dd9967c5b943d82e990047c28eb
David Melsheimer <dave.melshman@gmail.com>
2022-03-02 02:23:15 -0500

    piecing together sha256, then eventual animation in browser

--- sha256/add_binary.js
+++ sha256/add_binary.js
@@ -1,3 +1,5 @@
+const { download } = require("express/lib/response");
+
 function addArrayOfBinWords(arrayOfWords){
     //this is a novel way I came up with for doing binary math on multiple
     //binary words 'all at once'
@@ -16,10 +18,32 @@ function addArrayOfBinWords(arrayOfWords){
         evaluatedBinaryString.push(columnSum % 2);
         carryOver = Math.floor(columnSum/2);
     }
-    console.log(evaluatedBinaryString);
     return evaluatedBinaryString.reverse().join('');
 }
 
-let testArray = ['10100111', '11010001', '10000011', '10111101'];
-//parseInt(num.toString(2)[4]) + parseInt(num.toString(2)[4]);
-console.log(addArrayOfBinWords(testArray));
+function shiftRight(amount){
+    //ridiculous padding function to fill in the front with zeros
+    let padding = Array.apply(null, Array(amount)).map(function (pad) {return'0'}).join('');
+    return function(word){
+        let shifted = `${padding}${word.slice(0, -17)}`;
+        console.log(`original word :${word}`);
+        console.log(`${amount} Char right :${shifted}`);
+
+        // return `${padding}${word.slice(0, -17)}`;
+        return shifted;
+    }
+}
+
+const SR17 = shiftRight(17);
+let shifted = SR17('11100001011000100110001110001100');
+
+let testArray = ['11100001011000100110001110001100',
+                 '01100001011000000110001110000000',
+                 '01000100000000000000000010000000',
+                 '01100001011000101100000110000011'];
+
+// console.log(addArrayOfBinWords(testArray));
+
+// WHEN YOU SIT BACK DOWN FIGURE OUT TWO THINGS: 
+// IS THE SHIFT RIGHT SR FUNCTION REALLY HANDING INPUT AS BINARY AS IT SHOULD
+// WHATS UP WITH THE SIGN OVERFLOWING WHEN I USE ALL 32 BITS TO THE SR17 FUNCTION

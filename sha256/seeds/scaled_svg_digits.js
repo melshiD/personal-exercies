e91d45a24c52372891e53b1f2652f1b96c988a8d
David Melsheimer <dave.melshman@gmail.com>
2022-03-06 17:38:44 -0500

    still wrestling with large composite SVGs

--- sha256/seeds/scaled_svg_digits.js
+++ sha256/seeds/scaled_svg_digits.js
@@ -2,7 +2,7 @@ function buildScaledDigitDefs(baseSize, scale){
     let size = `${Math.floor(baseSize*scale)}px`;
     let symbolTree = document.getElementById('symbols');
     let svgns = 'http://www.w3.org/2000/svg';
-    
+
 }
 
 
@@ -16,9 +16,11 @@ function buildScaledDigitDefs(baseSize, scale){
             <rect width="30" height="30" fill="white" stroke="black"></rect>
             <text x="7.5" y="25" style="font-size:30">0</text>
         </g>
-            //WHEN YOU SIT BACK DOWN, BUILD A JS TO ADD THESE BUILDING BLOCK
-        //DEFINITIONS TO DOM, BUT BEING SIZED DYNAMICALLY (MAYBE AS NEEDED)
-        //TO AID MY EXAMPLE BUT WITHOUT USING SCALE TRANSFORMS
+            //WHEN YOU SIT BACK DOWN, REDESIGN FOR 0/1 BY CLASS (ALA GIT ANIMATION)
+            //AND ANOTHER WITH THE SEPARATE 0S AND 1S, BUT JUST SWITCHING THE USE HREF
+            //DESIGNING THIS WAY WILL ALLOW SHIFTS AND ROTATIONS WITHOUT
+            //USING INDIVIDUAL VIEWBOXES FOR EACH WORD.  alTHOGHT ID STILL LIKE
+            //FIGIRE THAT OUT
         <g id="one">
             <rect width="30" height="30" fill="white" stroke="black"></rect>
             <text x="7.5" y="25" style="font-size:30">1</text>

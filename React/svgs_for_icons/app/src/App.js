017f6446ddda4e0162947ca1443b77a88f9b7971
David Melsheimer <dave.melshman@gmail.com>
2022-03-12 23:54:24 -0500

    using refs for integrating gsap

--- React/svgs_for_icons/app/src/App.js
+++ React/svgs_for_icons/app/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import BouncingBallsIcon from './Components/Icons/BouncingBallsIcon';
+import React from 'react';
+import CustomTextInput from "./Components/Icons/CustomTextInput";
 
 function App() {
   return (
+    <React.Fragment>
     <BouncingBallsIcon />
+    <CustomTextInput />
+    </React.Fragment>
   );
 }
 
--- React/svgs_for_icons/app/src/Components/Icons/BouncingBallsIcon.js
+++ React/svgs_for_icons/app/src/Components/Icons/BouncingBallsIcon.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { createRef, useEffect, useRef } from 'react';
 import classes from './BouncingBallsIcon.module.css';
+import { gsap } from 'gsap/gsap-core';
+import { CSSPlugin } from 'gsap/CSSPlugin'
 
+
+// Force CSSPlugin to not get dropped during build
+gsap.registerPlugin(CSSPlugin)
 
 const BouncingBallsIcon = (props) => {
+    const rectRef = createRef();
+
+    useEffect( ()=> {
+        gsap.to(rectRef.current, {scale: '5'});
+    });
+
     return(
         <svg className={classes['svg-container']} viewBox='0 0 200 75'>
-            <rect x="0" y="0" width="20" height="20" fill="black" />
+            <rect ref={rectRef} x="30" y="20" width="30" rx='4' height="34" ></rect>
+            <circle id="c1" cx="10" cy="65" r="10" fill="black" />
+            <circle id="c2" cx="85" cy="55" r="20" fill="black" />
+            <circle id="c3" cx="170" cy="45" r="30" fill="black" />
         </svg>
     )
 };

9337271d79c8adb6f37725db0427282e340c66b2
David Melsheimer <dave.melshman@gmail.com>
2022-03-14 23:16:15 -0400

    how to animate viewBox from outside the func

--- React/svgs_for_icons/app/src/App.js
+++ React/svgs_for_icons/app/src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import BouncingBallsIcon from './Components/Icons/BouncingBallsIcon';
 import React from 'react';
-import CustomTextInput from "./Components/Icons/CustomTextInput";
 import classes from './App.module.css';
 import MovingViewBox from './Components/Icons/MovingViewBox';
 
--- React/svgs_for_icons/app/src/Components/Icons/MovingViewBox.js
+++ React/svgs_for_icons/app/src/Components/Icons/MovingViewBox.js
@@ -5,36 +5,74 @@ import { CSSPlugin } from 'gsap/CSSPlugin'
 import { Power4 } from 'gsap';
 // Force CSSPlugin to not get dropped during build
 gsap.registerPlugin(CSSPlugin)
-gsap.registerPlugin(CSSPlugin)
 
 const MovingViewBox = (props) => {
 
 
-    const el = useRef();
-    const q = gsap.utils.selector(el);
-    const tl = useRef();
+    const els1 = useRef();
+    const q = gsap.utils.selector(els1);
+    const els2 = useRef();
+    const r = gsap.utils.selector(els2);
+    const tl1 = useRef();
+    const tl2 = useRef();
 
     useEffect(() => {
-        tl.current = gsap.timeline()
+        tl1.current = gsap.timeline()
             .to(q('svg'), {
-                viewBox: '280 0 240 90',
-                duration: 4
+                attr: { viewBox: '0 0 240 90' },
+                duration: 4,
+                yoyo: true,
+                repeat: -1
+            }, 'beginning')
+            .to(q('svg'), {
+                attr: { viewBox: '480 0 240 90' },
+                duration: 4,
+                yoyo: true,
+                repeat: -1
             })
-            .to(q('svg'), {
-                viewBox: '0 0 240 90',
-                duration: 4
-            }).play();
     }, []);
-//WHEN YOU SIT BACK DOWN READ AND FIGURE OUT HOW TO ANIMATE VIEWBOX IN REACT
-    return (<div ref={el}>
-        <svg className={classes['svg-container']} >
-            <rect id="r1" x="10" y="65" width="10" height="20" rx="2" fill="black" />
-            <rect id="r2" x="85" y="55" width="20" height="20" rx="2" fill="black" />
-            <rect id="r3" x="170" y="45" width="30" height="20" rx="2" fill="black" />
-            <rect id="r3" x="170" y="45" width="30" height="20" rx="2" fill="black" />
-        </svg>
-    </div>
-    )
+
+    useEffect(() => {
+        tl2.current = gsap.timeline()
+            .set(r('#masking_rect'), {
+                x: -240
+            })
+            .to(r('#masking_rect'), {
+                x: 0,
+                duration: 4,
+                yoyo: true,
+                repeat: -1
+            }, 'beginning')
+            .to(r('#masking_rect'), {
+                x: 480,
+                duration: 4,
+                yoyo: true,
+                repeat: -1
+            })
+    }, []);
+
+    return (
+        <div ref={els1}>
+            <svg className={classes['svg-container']} viewBox="-240 0 240 90" ref={els2}>
+                <defs>
+                    {/* the color for the gradient should be configurable by prop  */}
+                    <linearGradient id="fade" >
+                        <stop offset="0%" stopColor="rgb(255, 255, 255)" stopOpacity="1" />
+                        <stop offset="5%" stopColor="rgb(255, 255, 255)" stopOpacity="0" />
+                        <stop offset="90%" stopColor="rgb(255, 255, 255)" stopOpacity="0" />
+                        <stop offset="100%" stopColor="rgb(255, 255, 255)" stopOpacity="1" />
+
+                    </linearGradient>
+
+                </defs>
+                <rect id="r1" x="10" y="65" width="10" height="20" rx="2" fill="black" />
+                <rect id="r2" x="85" y="55" width="20" height="20" rx="2" fill="black" />
+                <rect id="r3" x="170" y="45" width="30" height="20" rx="2" fill="black" />
+                <rect id="masking_rect" width="240" height="90" rx="2" fill="url(#fade)" />
+                {/* I really need to get this to work as a mask  */}
+            </svg>
+        </div>
+    );
 };
 
 export default MovingViewBox;

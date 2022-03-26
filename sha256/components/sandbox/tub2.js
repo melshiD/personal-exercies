b32daf128056d83bbcff0976f7db6f7b279670a3
David Melsheimer <dave.melshman@gmail.com>
2022-03-26 00:17:57 -0400

    animated the div content with transform digests

--- sha256/components/sandbox/tub2.js
+++ sha256/components/sandbox/tub2.js
@@ -23,24 +23,42 @@ const generateRotationSchedule = (inputWord, rotationSpecs) => {
     return transDigests;
 }
 
-let THREE_DIGESTS = generateRotationSchedule('11111111000000001111119100000000', 
-                                        [{rotation: 'ROTR', amount: 9}, 
-                                         {rotation: 'SHR', amount: 22},
+let THREE_DIGESTS = generateRotationSchedule('11111111000000001111110100000000', 
+                                        [{rotation: 'ROTR', amount: 13}, 
+                                         {rotation: 'ROTR', amount: 2},
                                          {rotation: 'SHR', amount: 23}]);
 
 let ONE_DIGEST = THREE_DIGESTS[0];
+let DIGEST_TWO = THREE_DIGESTS[1];
+let DIGEST_THREE = THREE_DIGESTS[2];
 
-const printWordsFromDigest = (digest, rotationDuration, index = 0) => { 
+const printWordsFromDigest = (digest, rotationDuration, index = 0, element) => { 
+    // console.log(index);
     if(index > digest.length - 1){
         return;
     }
-    console.log(digest[index]);
+    element.innerHTML = digest[index];
+    // console.log(digest[index]);
+
     setTimeout( () => {
-        printWordsFromDigest(digest, rotationDuration, index+1);
+        printWordsFromDigest(digest, rotationDuration, index+1, element);
     }, rotationDuration);
 }
 
-printWordsFromDigest(ONE_DIGEST, 200, 0);
+let bitDisplays = document.querySelectorAll('.bitDisplay');
+let element1 = bitDisplays[0];
+let element2 = bitDisplays[1];
+let element3 = bitDisplays[2];
+
+function double(rotTime){
+    let timeOne = Math.floor(rotTime/ONE_DIGEST.length);
+    let timeTwo = Math.floor(rotTime/DIGEST_TWO.length);
+    let timeThree = Math.floor(rotTime/DIGEST_THREE.length);
+    printWordsFromDigest(ONE_DIGEST, timeOne, 0, element1);
+    printWordsFromDigest(DIGEST_TWO, timeTwo, 0, element2);
+    printWordsFromDigest(DIGEST_THREE, timeThree, 0, element3);
+}
+// printWordsFromDigest(ONE_DIGEST, 200, 0);
 // const greeting = delayAmount => {
 //     setTimeout( () => {
 //         console.log('it rains today at ' + delayAmount);

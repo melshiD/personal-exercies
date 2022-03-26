49fc722ded62f247b9b58306fed2d81ee544be7b
David Melsheimer <dave.melshman@gmail.com>
2022-03-26 09:04:13 -0400

    automating and animating xor of transformations

--- sha256/components/sandbox/tub2.js
+++ sha256/components/sandbox/tub2.js
@@ -1,5 +1,5 @@
-//--------Single rotate/shift transform function-----------------
-function transformWordsAndReturnReceipt(inputBits, amount, flag){
+function transformWordAndReturnReceipt(inputBits, amount, flag){
+    //---------returns a 'transformation digest' for one word's transformation----
     let transformedDigest = [inputBits];
     let transformed = '';
     for(let i = 0; i < amount; i++){
@@ -15,15 +15,15 @@ function transformWordsAndReturnReceipt(inputBits, amount, flag){
     return transformedDigest;
 }
 
-const generateRotationSchedule = (inputWord, rotationSpecs) => {
+const generateRotationSchedules = (inputWord, rotationSpecs) => {
     let transDigests = [];
     for(let spec of rotationSpecs){
-        transDigests.push(transformWordsAndReturnReceipt(inputWord, spec.amount, spec.rotation));
+        transDigests.push(transformWordAndReturnReceipt(inputWord, spec.amount, spec.rotation));
     }
     return transDigests;
 }
 
-let THREE_DIGESTS = generateRotationSchedule('11111111000000001111110100000000', 
+let THREE_DIGESTS = generateRotationSchedules('11111111000000001111110100000000', 
                                         [{rotation: 'ROTR', amount: 13}, 
                                          {rotation: 'ROTR', amount: 2},
                                          {rotation: 'SHR', amount: 23}]);
@@ -58,14 +58,23 @@ function double(rotTime){
     printWordsFromDigest(DIGEST_TWO, timeTwo, 0, element2);
     printWordsFromDigest(DIGEST_THREE, timeThree, 0, element3);
 }
-// printWordsFromDigest(ONE_DIGEST, 200, 0);
-// const greeting = delayAmount => {
-//     setTimeout( () => {
-//         console.log('it rains today at ' + delayAmount);
-//         if(delayAmount <= 0){
-//             return;
-//         }
-//         greeting(Math.floor(delayAmount/2));
-//     }, delayAmount)
-// }
-// greeting(2000);
+
+
+//---------BINARY OPERATIONS------------------------------------
+function collapseArrayOfBinWords(arrayOfWords){
+    let collapsedWordsArray = [];
+    for(let i = arrayOfWords[0].length-1; i >= 0; i --){
+        let columnSum = 0;
+        arrayOfWords.forEach( (word) => columnSum += parseInt(word[i]) );
+        collapsedWordsArray.push(columnSum);
+    }
+    return collapsedWordsArray.reverse();
+}
+
+function exclusiveOr(arrayOfWords){
+    let collapsedWordsArray = collapseArrayOfBinWords(arrayOfWords);
+    for(let i = 0; i < collapsedWordsArray.length; i++){
+        collapsedWordsArray[i] = collapsedWordsArray[i]%2;
+    }
+    return collapsedWordsArray;
+}

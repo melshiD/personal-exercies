707ffd70c313ac5bd66dbefc7d87d0b077280fd2
David Melsheimer <dave.melshman@gmail.com>
2022-03-28 22:54:34 -0400

    need to animate the appending of the constant divs as well as their content updates

--- sha256/components/uppercaseSigma1/uppercaseSigmaOne.js
+++ sha256/components/uppercaseSigma1/uppercaseSigmaOne.js
@@ -1,10 +1,12 @@
+const { download } = require("express/lib/response");
+
 //-------------event listener for input  length----------------
 let inputBits = document.getElementById('bitsInput');
 inputBits.addEventListener('keyup', (event) => {
     lengthIndicator = event.target.value;
     inputBits.classList.add('incorrectLength');
     inputBits.classList.remove('correctLength');
-    if(inputBits.value.length === 32){
+    if (inputBits.value.length === 32) {
         inputBits.classList.remove('incorrectLength');
         inputBits.classList.add('correctLength');
     }
@@ -15,10 +17,12 @@ const grabRotationSpecElementsReturnSpecArray = (cardId) => {
     let specElementsList = document.querySelectorAll(`#${cardId} .transformationName`);
     const specObjectArray = () => {
         let specObjects = [];
-        for(let i = 0; i < specElementsList.length; i++){
+        for (let i = 0; i < specElementsList.length; i++) {
             specObjects.push(
-                {transformation: specElementsList[i].children[0].innerHTML, 
-                 degree: specElementsList[i].children[1].innerHTML}
+                {
+                    transformation: specElementsList[i].children[0].innerHTML,
+                    degree: specElementsList[i].children[1].innerHTML
+                }
             );
         }
         return specObjects;
@@ -28,21 +32,21 @@ const grabRotationSpecElementsReturnSpecArray = (cardId) => {
 
 const generateRotationSchedules = (inputWord, transformationSpecs) => {
     let transDigests = [];
-    for(let spec of transformationSpecs){
+    for (let spec of transformationSpecs) {
         transDigests.push(transformWordAndReturnReceipt(inputWord, spec.degree, spec.transformation));
     }
     return transDigests;
 }
 
-function transformWordAndReturnReceipt(inputBits, amount, flag){
+function transformWordAndReturnReceipt(inputBits, amount, flag) {
     //---------returns a 'transformation digest' for one word's transformation----
     let transformedDigest = [inputBits];
     let transformed = '';
-    for(let i = 0; i < amount; i++){
-        if(flag === 'ROTR'){
+    for (let i = 0; i < amount; i++) {
+        if (flag === 'ROTR') {
             transformed = `${inputBits.slice(-1)}${(inputBits.slice(0, 31))}`;
         }
-        if(flag === 'SHR'){
+        if (flag === 'SHR') {
             transformed = `${(inputBits.slice(0, -1)).padStart(32, '0')}`;
         }
         inputBits = transformed;
@@ -51,20 +55,20 @@ function transformWordAndReturnReceipt(inputBits, amount, flag){
     return transformedDigest;
 }
 
-const printWordsFromDigest = (digest, transformationDuration, index = 0, element, followOnFunction = null) => { 
+const printWordsFromDigest = (digest, transformationDuration, index = 0, element, followOnFunction = null) => {
     // console.log(index);
-    if(index > digest.length - 1){
-        if(!followOnFunction){return}
+    if (index > digest.length - 1) {
+        if (!followOnFunction) { return }
         return followOnFunction();
     }
     element.innerHTML = digest[index];
 
-    setTimeout( () => {
-        printWordsFromDigest(digest, transformationDuration, index+1, element);
+    setTimeout(() => {
+        printWordsFromDigest(digest, transformationDuration, index + 1, element);
     }, transformationDuration);
 }
 
-function handleAndRotateInput(rotTime, cardName){
+function handleAndRotateInput(rotTime, cardName) {
     let cardForTransformation = document.getElementById(cardName);
     let xorDisplayElement = cardForTransformation.querySelector('.resultBits');
     xorDisplayElement.innerHTML = '';
@@ -72,16 +76,16 @@ function handleAndRotateInput(rotTime, cardName){
     const transformationSpecs = grabRotationSpecElementsReturnSpecArray(cardName);
     let inputValue = document.getElementById('bitsInput').value;
     let schedules = generateRotationSchedules(inputValue, transformationSpecs)
-    let transTimeOne = Math.floor(rotTime/schedules[0].length);
-    let transTimeTwo = Math.floor(rotTime/schedules[1].length);
-    let transTimeThree = Math.floor(rotTime/schedules[2].length);
+    let transTimeOne = Math.floor(rotTime / schedules[0].length);
+    let transTimeTwo = Math.floor(rotTime / schedules[1].length);
+    let transTimeThree = Math.floor(rotTime / schedules[2].length);
     let elements = cardForTransformation.querySelectorAll(`.transformationExample`);
-    elements.forEach( elem => elem.innerHTML = inputValue);
+    elements.forEach(elem => elem.innerHTML = inputValue);
 
     let wordsToXor = [
-        schedules[0][schedules[0].length-1], 
-        schedules[1][schedules[1].length-1], 
-        schedules[2][schedules[2].length-1] 
+        schedules[0][schedules[0].length - 1],
+        schedules[1][schedules[1].length - 1],
+        schedules[2][schedules[2].length - 1]
     ]
     let xorValue = exclusiveOr(wordsToXor).join('');
     console.log(xorValue);
@@ -94,16 +98,16 @@ function handleAndRotateInput(rotTime, cardName){
             xorDisplayElement.innerHTML = xorValue;
         }, delay);
     }
-    setTimeout( () => printWordsFromDigest(schedules[1], transTimeTwo, 0, elements[1]), 200);
-    setTimeout( () => printWordsFromDigest(schedules[2], transTimeThree, 0, elements[2]), 0);
-    setTimeout( () => printWordsFromDigest(schedules[0], transTimeOne, 0, elements[0], 
+    setTimeout(() => printWordsFromDigest(schedules[1], transTimeTwo, 0, elements[1]), 200);
+    setTimeout(() => printWordsFromDigest(schedules[2], transTimeThree, 0, elements[2]), 0);
+    setTimeout(() => printWordsFromDigest(schedules[0], transTimeOne, 0, elements[0],
         printXorAtEndOfTransformation(420, xorValue, xorDisplayElement)
     ), 350);
 }
 
 
 // ------Constant Generation and Animation-------------------------
-function generateConstants(){
+function generateConstants() {
     //needs to go from 0-63 and do the following each iteration:
     //1. Build a constantRow with K(currentIndex): 3√prime(currentIndex)
     //2. Append the constantRow
@@ -113,82 +117,91 @@ function generateConstants(){
     //6. Move on to next element until finished
     const rawPrimes = compilePrimes(64);
     console.log(rawPrimes);
-    const cubedPrimes = rawPrimes.map( (primeValue) => Math.cbrt(primeValue));
+    const cubedPrimes = rawPrimes.map((primeValue) => Math.cbrt(primeValue));
     console.log(cubedPrimes);
-    const cubedPrimesLessWholeNumbers = cubedPrimes.map( cubedPrime => cubedPrime % 1 );
+    const cubedPrimesLessWholeNumbers = cubedPrimes.map(cubedPrime => cubedPrime.toString(10).split('.')[1]);
     console.log(cubedPrimesLessWholeNumbers);
-    const binaryCubedConstants = genCubedValues(rawPrimes);
-    console.log(binaryCubedConstants);
-    // const cubedPrimesDecimals = cubedPrimes.forEach( cubedPrime => cubedPrime.toString(10).split('.')[0] ); //already string value
-    // const cubedPrimesAsBinaryConstants = cubedPrimesDecimals.forEach( cubedPrimeDecimal => parseInt(cubedPrimeDecimal, 2).toString(2) );
+    const cubedPrimesAsBinary = genCubedValues(rawPrimes);
+    console.log(cubedPrimesAsBinary);
 
-    // console.log(cubedPrimesAsBinaryConstants);
-    // const parentElement = document.getElementById('constantCard');
-    // const populateAndAnimatePrimes = (parentToAppendTo, numberOfConstants) => {
-    //     for(let i = 0; i < rawPrimesArray.length; i++){
-    //         let constantIndexLable = `K(${i})`;
-    //         let thisPrimeDecimalValues = thisPrime.split('.')[1];
-    //         let binaryConstant = parseInt(thisPrimeDecimalValues, 2).substring(0, 32);
+    const parentElement = document.getElementById('constantCard');
+    const populateAndAnimatePrimes = (parentToAppendTo) => {
+        let constantContentsDisplayArrays = [];
+        for (let i = 0; i < rawPrimes.length; i++) {
+            constantContentsDisplayArrays.push(
+                [
+                    `3√prime(${i})`,
+                    `3√(${rawPrimes[i]})`,
+                    `${cubedPrimes[i]}`,
+                    `${cubedPrimesLessWholeNumbers[i]}`,
+                    `${cubedPrimesAsBinary[i]}`
+                ]
+            );
+        }
 
-    //         let constantContentsDisplayArray = [
-    //             `3√prime(${i})`,
-    //             `${thisPrime}`,
-    //             `${thisPrimeDecimalValues}`
-    //         ]
-    //     }
-    // }
-    let newConstantCardRow = document.querySelector('.constantCardRow').cloneNode(true);
-    let constantIndexNumber = newConstantCardRow.firstElementChild.children[0];
-    constantIndexNumber.innerHTML = 'K(23)';
-    let constantContents = newConstantCardRow.children[1];
-    constantContents.innerHTML = '11111111110000000000111000111111'
-    // parentElement.appendChild(newConstantCardRow);
+        const animateToBinary = (constantContentsDisplayArrays, constantContentsDisplayArray, displayIndex = 0) => {
+            let newConstantCardRow = parentToAppendTo.querySelector('.constantCardRow').cloneNode(true);
+            let constantIndexNumber = newConstantCardRow.firstElementChild.children[0];
+            constantIndexNumber.innerHTML = constantIndexLable;
+            let constantContents = newConstantCardRow.children[1];
+            constantContents.innerHTML = constantContentsDisplayArray[0];
+            parentElement.appendChild(newConstantCardRow);
+
+            // WHEN YOU SIT BACK DOWN BUILD A NESTED FUNCTION TO ANIMATE THE CASCADE OF APPENDING AND ANIMATING CONTENTS 
+            if (displayIndex >= constantContentsDisplayArray.length) { return }
+            constantContents.innerHTML = constantContentsDisplayArray[displayIndex];
+            setTimeout(animateThroughBinary, 1600, constantContents, constantContentsDisplayArray, displayIndex + 1);
+        }
+        animateToBinary(constantContents, constantContentsDisplayArray, 0);
+    }
+
+    populateAndAnimatePrimes(parentElement);
 }
 
 
 // ------SHA and Math functions---------------------------------------------
-function exclusiveOr(arrayOfWords){
+function exclusiveOr(arrayOfWords) {
     let collapsedWordsArray = collapseArrayOfBinWords(arrayOfWords);
-    for(let i = 0; i < collapsedWordsArray.length; i++){
-        collapsedWordsArray[i] = collapsedWordsArray[i]%2;
+    for (let i = 0; i < collapsedWordsArray.length; i++) {
+        collapsedWordsArray[i] = collapsedWordsArray[i] % 2;
     }
     return collapsedWordsArray;
 }
 
-function collapseArrayOfBinWords(arrayOfWords){
+function collapseArrayOfBinWords(arrayOfWords) {
     let collapsedWordsArray = [];
-    for(let i = arrayOfWords[0].length-1; i >= 0; i --){
+    for (let i = arrayOfWords[0].length - 1; i >= 0; i--) {
         let columnSum = 0;
-        arrayOfWords.forEach( (word) => columnSum += parseInt(word[i]) );
+        arrayOfWords.forEach((word) => columnSum += parseInt(word[i]));
         collapsedWordsArray.push(columnSum);
     }
     return collapsedWordsArray.reverse();
 }
 //---//---Prime Constant Generation Functions
-function genCubedValues(primeNumberArray){
+function genCubedValues(primeNumberArray) {
     let cubedConstants = [];
-    primeNumberArray.forEach( (prime) => {
-        cubedConstants.push( Math.cbrt(prime)
-        .toString(2).split('.')[1]
-        .substring(0, 32) );
+    primeNumberArray.forEach((prime) => {
+        cubedConstants.push(Math.cbrt(prime)
+            .toString(2).split('.')[1]
+            .substring(0, 32));
     });
     return cubedConstants
 }
 
-function compilePrimes(howMany){
+function compilePrimes(howMany) {
     let primeArray = [];
     let counter = 2;
-    while(primeArray.length < howMany){
-        if(isPrime(counter)){primeArray.push(counter)}
-        counter ++;
+    while (primeArray.length < howMany) {
+        if (isPrime(counter)) { primeArray.push(counter) }
+        counter++;
     }
     return primeArray;
 }
 
-function isPrime(n){
-    if(n < 2) return 0;
-    for(let i = 2; i < n; i++){
-        if(n % i === 0){
+function isPrime(n) {
+    if (n < 2) return 0;
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
             return 0;
         }
     }

8ef705ccdf2ba93379e48df3e7bcf8133af2b87a
David Melsheimer <dave.melshman@gmail.com>
2022-04-07 22:37:23 -0400

    how far to nest the promises

--- sha256/components/uppercaseSigma1/shaSpa.js
+++ sha256/components/uppercaseSigma1/shaSpa.js
@@ -203,7 +203,12 @@ function majorityAndChoice(evalDuration, cardName, inputBits = null){
         let resultsDiv = cardForTransformation.querySelector('.resultBits');
         for(let i = 0, p = Promise.resolve(); i < 32; i++){
             //remove all the activeClass added in a last iteration of the loop
-
+            // if(inputStringsAsArrays[0][i - 1]) threeListsOf32Spans[0].children[(31 - i - 1)].classList.remove('activeSpanDigitOne');
+            // if(inputStringsAsArrays[1][i - 1]) threeListsOf32Spans[1].children[(31 - i - 1)].classList.remove('activeSpanDigitOne');
+            // if(inputStringsAsArrays[2][i - 1]) threeListsOf32Spans[2].children[(31 - i - 1)].classList.remove('activeSpanDigitOne');
+            // if(inputStringsAsArrays[0][i - 1]) threeListsOf32Spans[0].children[(31 - i - 1)].classList.remove('activeSpanDigitZero');
+            // if(inputStringsAsArrays[1][i - 1]) threeListsOf32Spans[1].children[(31 - i - 1)].classList.remove('activeSpanDigitZero');
+            // if(inputStringsAsArrays[2][i - 1]) threeListsOf32Spans[2].children[(31 - i - 1)].classList.remove('activeSpanDigitZero');
             //'WHEN YOU SIT BACK DOWN, ADDRESS THE ABOVE COMMENT AND MAKE THE COLORS FLASH AND NOT PERSIST
             p = p.then( () => sequencePromise(evalDuration)).then( () => {
                 let majority = parseInt(inputStringsAsArrays[0][i], 10) + parseInt(inputStringsAsArrays[1][i], 10) + parseInt(inputStringsAsArrays[2][i], 10);
@@ -212,9 +217,9 @@ function majorityAndChoice(evalDuration, cardName, inputBits = null){
                 if(majority > 1){
                     //highlight the majority digit/s in that column;
                     for(let t = 0; t < 3; t ++){
-                        // if(inputStringsAsArrays[0][i] > 0) threeListsOf32Spans[0].children[(31 - i)].classList.add('activeSpanDigitOne');
-                        // if(inputStringsAsArrays[1][i] > 0) threeListsOf32Spans[1].children[(31 - i)].classList.add('activeSpanDigitOne');
-                        // if(inputStringsAsArrays[2][i] > 0) threeListsOf32Spans[2].children[(31 - i)].classList.add('activeSpanDigitOne');
+                        if(inputStringsAsArrays[0][i] > 0) threeListsOf32Spans[0].children[(31 - i)].classList.add('activeSpanDigitOne');
+                        if(inputStringsAsArrays[1][i] > 0) threeListsOf32Spans[1].children[(31 - i)].classList.add('activeSpanDigitOne');
+                        if(inputStringsAsArrays[2][i] > 0) threeListsOf32Spans[2].children[(31 - i)].classList.add('activeSpanDigitOne');
                     }
                     resultsDisplayOutput = `${'1'}${resultsDiv.innerHTML}`;
                     resultsDiv.innerHTML = `${resultsDisplayOutput}`;
@@ -222,9 +227,9 @@ function majorityAndChoice(evalDuration, cardName, inputBits = null){
                 if(majority < 2){
                     //highlight the majority digit/s in that column;
                     for(let t = 0; t < 3; t ++){
-                        // if(inputStringsAsArrays[0][i] == '0') threeListsOf32Spans[0].children[(31 - i)].classList.add('activeSpanDigitZero');
-                        // if(inputStringsAsArrays[1][i] == '0') threeListsOf32Spans[1].children[(31 - i)].classList.add('activeSpanDigitZero');
-                        // if(inputStringsAsArrays[2][i] == '0') threeListsOf32Spans[2].children[(31 - i)].classList.add('activeSpanDigitZero');
+                        if(inputStringsAsArrays[0][i] == '0') threeListsOf32Spans[0].children[(31 - i)].classList.add('activeSpanDigitZero');
+                        if(inputStringsAsArrays[1][i] == '0') threeListsOf32Spans[1].children[(31 - i)].classList.add('activeSpanDigitZero');
+                        if(inputStringsAsArrays[2][i] == '0') threeListsOf32Spans[2].children[(31 - i)].classList.add('activeSpanDigitZero');
                     }
                     resultsDisplayOutput = `${'0'}${resultsDiv.innerHTML}`;
                     resultsDiv.innerHTML = `${resultsDisplayOutput}`;

e88db532cb7774daad1064194e85b3adc830a5d6
David Melsheimer <dave.melshman@gmail.com>
2022-04-11 13:11:17 -0400

    slogging

--- sha256/components/sandbox/backToBasics.js
+++ sha256/components/sandbox/backToBasics.js
@@ -1,3 +1,5 @@
+const res = require("express/lib/response");
+
 //in an effort to make well-formed timelines with chained promises and setTimeouts
 function sayBlue(){
     console.log('blue');
@@ -9,13 +11,28 @@ function sayGreen(){
     console.log('green');
 }
 
-function delayFunction(delayTime, delayFunc, necessaryItems = null){
-    return new Promise( (resolve) =>{
-        setTimeout(delayFunc, delayTime);
-    })
+//Well, as you know a Promise really just calls a resolve function or reject function after something happens.
+
+const asyncFoo = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log('nice');
+    console.log('way better');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log('and thats a wrap')
 }
 
-delayFunction(1000, sayBlue)
-    .then( 
-        delayFunction(1000, sayPink)
-    );
+
+const resFunc = () => console.log('resolving log func');
+const sleep = (ms, resolvingFunc) =>{
+    return new Promise(flobsDizzle => setTimeout(
+        flobsDizzle, ms));
+}
+
+async function asyncBar(ms){
+    console.log('did it...');
+    await sleep(ms, resFunc);
+    console.log('doe');
+    await sleep(ms, resFunc);
+    console.log('and again');
+}
+asyncBar(1000);

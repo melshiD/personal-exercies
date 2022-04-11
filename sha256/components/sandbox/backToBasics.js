0e4aa1a67a4069409443b48901c500b6aa640785
David Melsheimer <dave.melshman@gmail.com>
2022-04-10 23:54:13 -0400

    returning promises... keeping promises and maybe breaking promises

--- sha256/components/sandbox/backToBasics.js
+++ sha256/components/sandbox/backToBasics.js
@@ -1,1 +1,21 @@
 //in an effort to make well-formed timelines with chained promises and setTimeouts
+function sayBlue(){
+    console.log('blue');
+}
+function sayPink(){
+    console.log('pink');
+}
+function sayGreen(){
+    console.log('green');
+}
+
+function delayFunction(delayTime, delayFunc, necessaryItems = null){
+    return new Promise( (resolve) =>{
+        setTimeout(delayFunc, delayTime);
+    })
+}
+
+delayFunction(1000, sayBlue)
+    .then( 
+        delayFunction(1000, sayPink)
+    );

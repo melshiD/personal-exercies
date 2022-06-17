551c8bf6a90b1dcfbc450ea1d31de9236460f758
David Melsheimer <dave.melshman@gmail.com>
2022-06-16 23:53:53 -0400

    figured out how to account for position and empty spaces on board using single useState hook

--- rapture/rapture/src/App.js
+++ rapture/rapture/src/App.js
@@ -6,14 +6,21 @@ import MainContainer from './components/MainContainer';
 
 function App() {
 
-  const [clickedDie, setClickedDie] = useState();
+  let pos1, pos2 = '';
+
+  const [clickedDie, setClickedDie] = useState({
+    pos1: "12", pos2: "11", pos3: "14", pos4: "13"
+  });
 
   const clickHandler = (event) => {
-    let regex = new RegExp(/[0-22]/);
-    setClickedDie(event.target.getAttribute('href').match(regex));
-    console.log(clickedDie);
+    let regex = new RegExp(/\d{1,2}/);
+    // setClickedDie(() => {
+    //   return event.target.getAttribute('href').match(regex)[0];
+    // });
+    setClickedDie(event.target.getAttribute('href').match(regex)[0]);
+    // console.log(clickedDie);
   }
-
+  console.log(clickedDie);
   return (
     <MainContainer>
 
@@ -22,10 +29,13 @@ function App() {
       </h1>
         <FaceImageDefinitions />
         <DiceContainer>
-          <DieDiv dieNumber="12" onMouseDown={clickHandler}/>
-          <DieDiv dieNumber='6' onMouseDown={clickHandler}/>
-          <DieDiv dieNumber="4" onMouseDown={clickHandler}/>
-          <DieDiv dieNumber="21" onMouseDown={clickHandler}/>
+          <DieDiv dieNumber={clickedDie.pos1} onMouseDown={clickHandler}/>
+          <DieDiv dieNumber={clickedDie.pos2} onMouseDown={clickHandler}/>
+          <DieDiv dieNumber={clickedDie.pos3} onMouseDown={clickHandler}/>
+
+          {/* WHEN YOU SIT BACK DOWN, USE THE RETURN FUNCTION TO SPREAD/UPDATE THE FULL STATE WITH CLICKHANDLER  */}
+          {/* see below... if a position isn't accounted for in state, it means the dice no longer fills that space */}
+          {clickedDie.pos4 && <DieDiv dieNumber={clickedDie.pos4} onMouseDown={clickHandler}/>}
         </DiceContainer>
         <DiceContainer>
           <DieDiv dieNumber="1" onMouseDown={clickHandler}/>
